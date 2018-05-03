99c21105529be0801d7908f2fa09896a83ce4135
JewsOfHazard <logan.saso2016@gmail.com>
2018-05-02 23:54:55 -0700

    Spaces

--- src/audio_devices.rs
+++ src/audio_devices.rs
@@ -4,92 +4,92 @@ use rocket::{http::RawStr, request::FromParam};
 use std::process::Command;
 
 pub enum AudioDeviceType {
-	Input,
-	Output,
+    Input,
+    Output,
 }
 
 impl<'a> FromParam<'a> for AudioDeviceType {
-	type Error = String;
+    type Error = String;
 
-	fn from_param(param: &'a RawStr) -> Result<Self, Self::Error> {
-		match param.as_str() {
-			"input" => Ok(AudioDeviceType::Input),
-			"output" => Ok(AudioDeviceType::Output),
-			_ => Err(String::from("No device of that type.")),
-		}
-	}
+    fn from_param(param: &'a RawStr) -> Result<Self, Self::Error> {
+        match param.as_str() {
+            "input" => Ok(AudioDeviceType::Input),
+            "output" => Ok(AudioDeviceType::Output),
+            _ => Err(String::from("No device of that type.")),
+        }
+    }
 }
 
 #[get("/devices/<device_type>/default")]
 pub fn default_audio_device_type(device_type: AudioDeviceType) -> String {
-	let device = match device_type {
-		AudioDeviceType::Output => cpal::default_output_device(),
-		AudioDeviceType::Input => cpal::default_input_device(),
-	};
+    let device = match device_type {
+        AudioDeviceType::Output => cpal::default_output_device(),
+        AudioDeviceType::Input => cpal::default_input_device(),
+    };
 
-	if let Some(device) = device {
-		device.name()
-	} else {
-		String::from("No supported default audio device.")
-	}
+    if let Some(device) = device {
+        device.name()
+    } else {
+        String::from("No supported default audio device.")
+    }
 }
 
 #[get("/devices")]
 pub fn audio_devices() -> String {
-	let devices = cpal::devices();
+    let devices = cpal::devices();
 
-	let mut devices_string: String = String::new();
-	for device in devices {
-		devices_string.push_str(format!("{}\n", device.name()).as_str());
-	}
+    let mut devices_string: String = String::new();
+    for device in devices {
+        devices_string.push_str(format!("{}\n", device.name()).as_str());
+    }
 
-	devices_string
+    devices_string
 }
 
 #[get("/devices/<device_type>")]
 pub fn audio_device_type(device_type: AudioDeviceType) -> String {
-	let devices = match device_type {
-		AudioDeviceType::Output => cpal::output_devices(),
-		AudioDeviceType::Input => cpal::input_devices(),
-	};
+    let devices = match device_type {
+        AudioDeviceType::Output => cpal::output_devices(),
+        AudioDeviceType::Input => cpal::input_devices(),
+    };
 
-	let mut devices_string: String = String::new();
-	for device in devices {
-		devices_string.push_str(format!("{}\n", device.name()).as_str());
-	}
+    let mut devices_string: String = String::new();
+    for device in devices {
+        devices_string.push_str(format!("{}\n", device.name()).as_str());
+    }
 
-	devices_string
+    devices_string
 }
 
 #[cfg(target_family = "windows")]
 #[get("/devices/<device_type>/default/<name>")]
 pub fn set_default_audio_device(device_type: AudioDeviceType, name: String) -> String {
-	let assign_type = match device_type {
-		AudioDeviceType::Output => 1,
-		AudioDeviceType::Input => 2,
-	};
+    let assign_type = match device_type {
+        AudioDeviceType::Output => 1,
+        AudioDeviceType::Input => 2,
+    };
 
-	let devices = cpal::devices();
+    let devices = cpal::devices();
 
-	let _ = Command::new("nircmd")
-		.args(&[
-			"setdefaultsounddevice",
-			name.as_str(),
-			assign_type.to_string().as_str(),
-		])
-		.output();
+    let _ = Command::new("nircmd")
+        .args(&[
+            "setdefaultsounddevice",
+            name.as_str(),
+            assign_type.to_string().as_str(),
+        ])
+        .output();
 
-	if devices.map(|device| device.name()).any(|device_name| {
-		println!("Checking {} against {}", device_name, name);
-		device_name == name
-	}) {
-		String::from("Audio device changed.")
-	} else {
-		//This else is always called because device.name() returns the full concatenated name
-		//Nircmd only takes display name
-		//So for "Realtek High Definition Audio(Optical) (Optical Output Device)"
-		//Nircmd only works with "Realtek High Definition Audio(Optical)" but cpal returns the full name
-		String::from("Audio device changed.")
-		// String::from(format!("{} is not a valid audio device.", name))
-	}
+    if devices.map(|device| device.name()).any(|device_name| {
+        println!("Checking {} against {}", device_name, name);
+        device_name == name
+    }) {
+        String::from("Audio device changed.")
+    } else {
+        //This else is always called because device.name() returns the full concatenated name
+        //Nircmd only takes display name
+        //So for "Realtek High Definition Audio(Optical) (Optical Output Device)"
+        //Nircmd only works with "Realtek High Definition Audio(Optical)" but cpal returns the full name
+        String::from("Audio device changed.")
+        // String::from(format!("{} is not a valid audio device.", name))
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,33 @@
 #![feature(use_extern_macros)]
 #![plugin(rocket_codegen)]
 
-extern crate rocket;
 extern crate cpal;
+extern crate rocket;
 
 mod audio_devices;
 use audio_devices::*;
 
 use rocket::Rocket;
-use std::process::{Command, Output};
 use std::io::Result;
-
+use std::process::{Command, Output};
 
 #[get("/shutdown")]
 fn shutdown() -> String {
+	let result: Result<Output> = Command::new("shutdown").args(&["/p"]).output();
 
-	let result: Result<Output> = Command::new("shutdown")
-			.args(&["/p"])
-			.output();
-	
 	match result {
-		Ok(x) => String::from(format!("Process exited with code {} - and output {}", x.status, std::str::from_utf8(&x.stdout[0..x.stdout.len()]).expect("Status output wasn't in UTF8, for some reason."))),
-		Err(z) => String::from(format!("Process exited with error code {:?}", z.raw_os_error())),
+		Ok(x) => String::from(format!(
+			"Process exited with code {} - and output {}",
+			x.status,
+			std::str::from_utf8(&x.stdout[0..x.stdout.len()])
+				.expect("Status output wasn't in UTF8, for some reason.")
+		)),
+		Err(z) => String::from(format!(
+			"Process exited with error code {:?}",
+			z.raw_os_error()
+		)),
 	}
-
 }
-
 
 fn rocket() -> Rocket {
 	rocket::ignite()
@@ -34,18 +36,19 @@ fn rocket() -> Rocket {
 
 #[cfg(target_family = "windows")]
 fn main() {
-
 	rocket()
 		.mount("/", routes![shutdown])
 		.mount("/audio", routes![audio_devices, audio_device_type, default_audio_device_type])
 		.mount("/audio", routes![set_default_audio_device]) //Windows only (currently)
 			.launch();
-
 }
 
 #[cfg(target_family = "unix")]
 fn main() {
-		rocket()
-		.mount("/audio", routes![audio_devices, audio_device_type, default_audio_device_type])
-			.launch();
+	rocket()
+		.mount(
+			"/audio",
+			routes![audio_devices, audio_device_type, default_audio_device_type],
+		)
+		.launch();
 }
